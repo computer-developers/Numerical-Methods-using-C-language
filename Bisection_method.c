1da275c6867b86bed766a58d6f3d47fe70e50271
unknown <neel.patel.2012.np@gmail.com>
2016-02-04 18:58:01 +0530

    remove method to find initial limites

--- Bisection_method.c
+++ Bisection_method.c
@@ -1,62 +1,61 @@
 #include<stdio.h>
 #include<conio.h>
-#define deci 0.01
-float c1,c2,c3,l,u,m;
+float l,u,m;
 float f(float);
-void start();
 void bisect(float,float);
+int scan();
 void main()
 {
-	printf("enter value of co-efficient...");
-	scanf("%f %f %f",&c1,&c2,&c3);
-	start();
+	printf("enter limits...");
+	for(;!scan(););
 	bisect(l,u);
-	printf("root=%.2f",m);
+	printf("\nanswer=%f",m);
 }
 float f(float x)
 {
 	float y;
-	y=x*x*x*c1+x*c2+c3;
+	y=x*x*x-x-11;
 	return y;
 }
-void start()
+int scan()
 {
-	int i;
-	if(f(0)==0)
+	scanf("%f %f",&l,&u);
+	if(f(u)<0)
 	{
-		l=0;
-		u=0;
-		m=0;
-		return;
-	}
-	if(f(0)>0)
-	{
-		u=0;
-		for(i=1;f(i)>0&&f(-i)>0;i++);
-		if(f(i)<0)
-			l=i;
+		if(f(l)<0)
+			return 0;
 		else
-			l=-i;
-		return;
+			{
+				l=l+u;
+				u=l-u;
+				l=l-u;
+				return 1;
+			}
 	}
-	if(f(0)<0)
+	if(f(u)>0)
 	{
-		l=0;
-		for(i=1;f(i)<0&&f(-i)<0;i++);
-		if(f(i)>0)
-			u=i;
+		if(f(l)>0)
+			return 0;
 		else
-			u=-i;
-		return;
+			return 1;	
+	}
+	else
+	{
+		m=u;
+		return 1;
 	}
 }
 void bisect(float a,float b)
 {
-	printf("\n l=%f u= %f m= %f f(m)=%f",l,u,m,f(m));
-	if(a-b<deci&&a-b>-deci)
+	int x;
+	x=a*10-b*10;
+	if(x==0)
 		return;
 	m=(a+b)/2;
 	if(f(m)>0)
 		bisect(a,m);
-	else	bisect(m,b);
+	else if(f(m)<0)
+		bisect(m,b);
+	else
+		return;
 }
