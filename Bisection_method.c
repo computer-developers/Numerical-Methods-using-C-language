1177bb075e69de8f656f462989b5a48a453f369e
unknown <neel.patel.2012.np@gmail.com>
2016-02-04 19:32:36 +0530

    Add method to scan function at run time.

--- Bisection_method.c
+++ Bisection_method.c
@@ -1,61 +1,218 @@
 #include<stdio.h>
 #include<conio.h>
-float l,u,m;
+#define DECI 3
+struct eq
+{
+	float c;
+	int p;
+	int s;
+	struct eq *l;
+}*first=NULL;
+float c1,c2,c3,l,u,m;
 float f(float);
-void bisect(float,float);
+void start();
+int ip(char);
 int scan();
+void bisect(float,float);
 void main()
 {
-	printf("enter limits...");
-	for(;!scan(););
+	if(!scan())return;
+	struct eq *h;
+	h=first;
+	for(;h!=NULL;)
+	{
+		printf("\ns=%d c=%f p=%d",h->s,h->c,h->p);
+		h=h->l; 
+	}
+	start();
 	bisect(l,u);
-	printf("\nanswer=%f",m);
+	printf("\nF(0) = %f root = %.3f f(m)=%f",f(0),m,f(m));
 }
 float f(float x)
 {
-	float y;
-	y=x*x*x-x-11;
+	float y=0;
+	struct eq *t;
+	t=first;
+	for(;t!=NULL;)
+	{
+		y=y+((t->c) * (t->s) * pow(x,t->p));
+		t=t->l; 
+	}
 	return y;
 }
-int scan()
+void start()
 {
-	scanf("%f %f",&l,&u);
-	if(f(u)<0)
+	int i;
+	if(f(0)==0)
 	{
-		if(f(l)<0)
-			return 0;
-		else
-			{
-				l=l+u;
-				u=l-u;
-				l=l-u;
-				return 1;
-			}
+		l=0;
+		u=0;
+		m=0;
+		return;
 	}
-	if(f(u)>0)
+	if(f(0)>0)
 	{
-		if(f(l)>0)
-			return 0;
+		u=0;
+		for(i=1;f(i)>0&&f(-i)>0;i++);
+		if(f(i)<0)
+			l=i;
 		else
-			return 1;	
+			l=-i;
+		return;
 	}
-	else
+	if(f(0)<0)
 	{
-		m=u;
-		return 1;
+		l=0;
+		for(i=1;f(i)<0&&f(-i)<0;i++);
+		if(f(i)>0)
+			u=i;
+		else
+			u=-i;
+		return;
 	}
 }
 void bisect(float a,float b)
 {
-	int x;
-	x=a*10-b*10;
-	if(x==0)
+	if(a-b<pow(0.1,DECI)&&b-a<pow(0.1,DECI))
 		return;
 	m=(a+b)/2;
 	if(f(m)>0)
 		bisect(a,m);
-	else if(f(m)<0)
-		bisect(m,b);
-	else
-		return;
+	else	bisect(m,b);
+}
+int scan()
+{
+	int k;
+	char c;
+	struct eq *t=NULL,*h=NULL;
+	printf("enter polynominal equation in x...");
+		for(;!ip(c=getche()););
+start:
+		h=(struct eq *)malloc(sizeof(struct eq));
+		h->l=NULL;
+		h->p=0;
+		h->s=1;
+		h->c=1;
+		if(t==NULL)
+		{
+			t=h;
+		}
+		else
+		{
+			t->l=h;
+			t=t->l;
+		}
+		if(first==NULL)
+			first=t;
+		if(ip(c)==5||ip(c)==4)
+		{
+			goto err;
+		}
+		else if(ip(c)==1)
+		{
+			t->s=1;
+			k=c-48;
+			goto num; 
+		}
+		else if(ip(c)==2)
+		{
+			if(c=='-')
+				t->s=-1;
+			else
+				t->s=1;
+			k=1;
+			goto sym;
+		}
+		else if(ip(c)==3)
+		{
+			t->s=1;
+			t->c=1;
+			goto var;
+		}
+num:		
+		if(!ip(c=getche()))
+		{
+			t->c=k;
+			return 1;
+		}
+		else if(ip(c)==1)
+		{
+			k=k*10+c-48;
+			goto num; 
+		}
+		else if(ip(c)==2)
+		{
+			goto start;
+		}
+		else if(ip(c)==3)
+		{
+			t->c=k;
+			goto var;
+		}
+		if(ip(c)==5||ip(c)==4)
+		{
+			goto err;
+		}
+sym:		
+		for(;!ip(c=getche()););
+		if(ip(c)==1)
+		{
+			k=c-48;
+			goto num; 
+		}
+		else if(ip(c)==3)
+		{
+			t->c=1;
+			goto var;
+		}
+		if(ip(c)==5||ip(c)==4||ip(c)==2)
+		{
+			goto err;
+		}
+var:
+		t->p=1;
+		if(!ip(c=getche()))return 1;
+		else if(ip(c)==2)
+		{
+			goto start; 
+		}
+		else if(ip(c)==4)
+		{
+			goto exp;
+		}
+		if(ip(c)==1||ip(c)==3||ip(c)==5)
+		{
+			goto err;
+		}
+exp:
+		for(;!ip(c=getche()););
+		if(ip(c)==1)
+			t->p=c-48;
+		else
+			goto err;
+		if(!ip(c=getche()))return 1;
+		else if(ip(c)==2)
+		{
+			goto start; 
+		}
+		if(ip(c)==5||ip(c)==4||ip(c)==1||ip(c)==3)
+		{
+			goto err;
+		}
+err:	printf("\ninvalid equation...");
+		return 0;
+}
+int ip(char c)
+{
+	if(c>='0'&&c<='9')
+	 	return 1;
+	if(c=='+'||c=='-')
+		return 2;
+	if(c=='X'||c=='x')
+		return 3;
+	if(c=='^'||c=='$')
+		return 4;
+	if(c==13)
+		return 0;
+	return 5;
 }
